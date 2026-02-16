8c335afb0e7c5b4761a781f7d076266ea09c8e99
Timothy <timothy.pearsonn1@gmail.com>
2026-02-16 19:48:56 +0100

    0 error for serial + openmp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <Kokkos_SIMD.hpp>
 #include <impl/Kokkos_InitializeFinalize.hpp>
 #include <iostream>
-using simd_t = Kokkos::Experimental::simd<int>;
+using simd_t = Kokkos::Experimental::simd<double>;
 
-void print_error(Kokkos::View<int *> y) {
+void print_error(Kokkos::View<double *> y, int n) {
   int error = 0.0;
 
-  for (int i = 0; i < y.extent(0); i++) {
+  for (int i = 0; i < n; i++) {
     error += std::abs(11 - y(i));
     std::cout << "y = " << y(i) << '\n';
   }
@@ -16,39 +16,38 @@ void print_error(Kokkos::View<int *> y) {
   std::cout << "Error: " << error << std::endl;
 }
 void test_simd(int N_in, int a) {
+  const int simd_size = simd_t::size();
+  int N = (N_in + simd_size - 1) / simd_size;
 
-  int N = N_in / simd_t::size();
-
-  Kokkos::View<int *> x("x", N);
-  Kokkos::View<int *> y("y", N);
+  Kokkos::View<double *> x("x", N * simd_size);
+  Kokkos::View<double *> y("y", N * simd_size);
 
   Kokkos::parallel_for(
-      "init", N, KOKKOS_LAMBDA(const int i) {
+      "init", N * simd_size, KOKKOS_LAMBDA(const int i) {
         y(i) = 2;
         x(i) = 3;
       });
   Kokkos::fence();
-  Kokkos::Timer timer;
-  timer.reset();
 
+  Kokkos::Timer timer;
   Kokkos::parallel_for(
-      "saxpy", N / simd_t::size(), KOKKOS_LAMBDA(int i) {
-        std::size_t idx = i * simd_t::size();
-        simd_t x_(&x(idx), Kokkos::Experimental::simd_flag_default);
-        simd_t y_(&y(idx), Kokkos::Experimental::simd_flag_default);
+      "simd_saxpy", N, KOKKOS_LAMBDA(const int i) {
+        simd_t x_(&x(i * simd_size), Kokkos::Experimental::simd_flag_default);
+        simd_t y_(&y(i * simd_size), Kokkos::Experimental::simd_flag_default);
         y_ = y_ + x_ * a;
         Kokkos::Experimental::simd_unchecked_store(
-            y_, &y(idx), Kokkos::Experimental::simd_flag_default);
+            y_, &y(i * simd_size), Kokkos::Experimental::simd_flag_default);
       });
+  Kokkos::fence();
   double time = timer.seconds();
   std::cout << "simd time = " << time << '\n';
-  print_error(y);
+  print_error(y, N_in);
 }
 
 void test_scalar(int N, int a) {
 
-  Kokkos::View<int *> y("y data", N);
-  Kokkos::View<int *> x("x data", N);
+  Kokkos::View<double *> y("y data", N);
+  Kokkos::View<double *> x("x data", N);
 
   Kokkos::parallel_for("init y", N, KOKKOS_LAMBDA(const int i) { y(i) = 2; });
   Kokkos::parallel_for("init x", N, KOKKOS_LAMBDA(const int i) { x(i) = 3; });
@@ -62,7 +61,7 @@ void test_scalar(int N, int a) {
 
   double time = timer.seconds();
   std::cout << "scalar time = " << time << '\n';
-  print_error(y);
+  print_error(y, N);
 }
 
 int main(int argc, char *argv[]) {
@@ -73,7 +72,7 @@ int main(int argc, char *argv[]) {
   int a = 3;
   std::cout << "N = " << N << '\n';
   std::cout << "a = " << a << '\n';
-  std::cout << "simd size" << simd_t::size() << '\n';
+  std::cout << "simd size: " << simd_t::size() << '\n';
 
   test_scalar(N, a);
   test_simd(N, a);
